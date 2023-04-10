996de5e013d03221e624d9cccb3d1c8f1995bb3e
super mario <mario160@mail.ru>
2023-04-10 12:01:13 +0300

    Working schedule update

--- forms/main.py
+++ forms/main.py
@@ -4,6 +4,7 @@ from tkinter import *
 import tkinter.ttk as ttk
 import tkinter.simpledialog as simpledialog
 import tkinter.messagebox as messagebox
+from datetime import time, datetime
 
 cnx = mysql.connector.connect(
     user=config.username,
@@ -23,6 +24,9 @@ def main_window():
     professors_button.pack(pady=10)
 
     students_button = Button(window, text="Список студентів", command=students_window)
+    students_button.pack(pady=10)
+
+    students_button = Button(window, text="Додати пару", command=add_schedule_element)
     students_button.pack(pady=10)
 
     window.mainloop()
@@ -212,6 +216,11 @@ ORDER BY Schedule.DayOfWeek, Schedule.StartTime"""
 
     # Insert data into the table
     for row in rows:
+        row = list(row)
+        row[-1] = [
+            "Понеділок", "Вівторок", "Середа", "Четвер", "П'ятниця", "Субота",
+            "Неділя"
+        ][row[-1]]
         table.insert("", "end", values=row)
 
     table.grid(row=1, column=0, columnspan=2)
@@ -261,6 +270,193 @@ def students_window():
     table.pack(padx=10, pady=10)
 
 
+def get_subjects():
+    cursor.execute("SELECT SubjectId, SubjectName FROM Subjects")
+    # Отримуємо результат запиту
+    result = cursor.fetchall()
+
+    ids = []
+    values = []
+    for k, v in result:
+        ids.append(k)
+        values.append(v)
+    return ids, values
+
+
+def get_groups():
+    cursor.execute("SELECT GroupId, GroupName FROM new_schema.Groups")
+    # Отримуємо результат запиту
+    result = cursor.fetchall()
+    # Повертаємо список груп
+    ids = []
+    values = []
+    for k, v in result:
+        ids.append(k)
+        values.append(v)
+    return ids, values
+
+
+def get_professors():
+    cursor.execute("SELECT ProfessorId, CONCAT(LastName, ' ', FirstName) FROM Professors")
+    # Отримуємо результат запиту
+    result = cursor.fetchall()
+    # Повертаємо список викладачів
+    ids = []
+    values = []
+    for k, v in result:
+        ids.append(k)
+        values.append(v)
+    return ids, values
+
+
+def get_classrooms():
+    cursor.execute("SELECT ClassroomId, ClassroomName FROM Classrooms")
+    # Отримуємо результат запиту
+    result = cursor.fetchall()
+    # Повертаємо список аудиторій
+    ids = []
+    values = []
+    for k, v in result:
+        ids.append(k)
+        values.append(v)
+    return ids, values
+
+
+def add_schedule_element():
+    add_schedule_window = Toplevel(window)
+    add_schedule_window.title("Додати нову пару")
+    add_schedule_window.geometry("300x400")
+
+    subject_label = Label(add_schedule_window, text="Предмет:")
+    subject_label.pack()
+    subject_ids, subject_names = get_subjects()
+    subject_entry = ttk.Combobox(add_schedule_window, values=subject_names)
+    subject_entry.pack()
+
+    group_label = Label(add_schedule_window, text="Група:")
+    group_label.pack()
+    groups_ids, groups_names = get_groups()
+    group_entry = ttk.Combobox(add_schedule_window, values=groups_names)
+    group_entry.pack()
+
+    professor_label = Label(add_schedule_window, text="Викладач:")
+    professor_label.pack()
+    professors_ids, professors_names = get_professors()
+    professor_entry = ttk.Combobox(add_schedule_window, values=professors_names)
+    professor_entry.pack()
+
+    classroom_label = Label(add_schedule_window, text="Аудиторія:")
+    classroom_label.pack()
+    classrooms_ids, classrooms_names = get_classrooms()
+    classroom_entry = ttk.Combobox(add_schedule_window, values=classrooms_names)
+    classroom_entry.pack()
+
+
+    # # Час початку
+    # start_time_frame = Frame(add_schedule_window)
+    # start_time_frame.pack()
+    # Label(start_time_frame, text="Час початку:").pack(side=TOP)
+
+    # start_time_frame_picking = Frame(start_time_frame)
+    # start_time_frame_picking.pack(side=TOP)
+    # start_time_hours = Spinbox(start_time_frame_picking, from_=0, to=23, width=5)
+    # start_time_hours.pack(side=LEFT, padx=5, pady=5)
+    # Label(start_time_frame_picking, text=":").pack(side=LEFT)
+    # start_time_minutes = Spinbox(start_time_frame_picking, from_=0, to=59, width=5)
+    # start_time_minutes.pack(side=LEFT, padx=5, pady=5)
+
+    # # Час закінчення
+    # end_time_frame = Frame(add_schedule_window)
+    # end_time_frame.pack()
+    # Label(end_time_frame, text="Час закінчення:").pack(side=TOP)
+
+    # end_time_frame_picking = Frame(end_time_frame)
+    # end_time_frame_picking.pack(side=TOP)
+    # end_time_hours = Spinbox(end_time_frame_picking, from_=0, to=23, width=5, validate="key",
+    #         #validatecommand=
+    # )
+    # end_time_hours.pack(side=LEFT, padx=5, pady=5)
+    # Label(end_time_frame_picking, text=":").pack(side=LEFT)
+    # end_time_minutes = Spinbox(end_time_frame_picking, from_=0, to=59, width=5)
+    # end_time_minutes.pack(side=LEFT, padx=5, pady=5)
+
+    time_label = Label(add_schedule_window, text="Номер пари:")
+    time_label.pack()
+    time_entry = ttk.Combobox(add_schedule_window, values=[f"{i} пара" for i in range(1, 5)])
+    time_entry.pack()
+
+    start_label = Label(add_schedule_window, text="Початок:")
+    start_label.pack()
+    end_label = Label(add_schedule_window, text="Кінець:")
+    end_label.pack()
+
+    def on_time_selected(e):
+        times = ["8:40", "10:15", "10:35", "12:10", "12:20", "13:55", "14:05", "15:40"]
+        if time_entry.current() == -1:
+            return
+        start_label.config(text=f"Початок: {times[2*time_entry.current()]}")
+        end_label.config(text=f"Кінець: {times[2*time_entry.current() + 1]}")
+    time_entry.bind("<<ComboboxSelected>>", on_time_selected)
+
+    print(group_entry.current())
+
+    day_of_week_label = Label(add_schedule_window, text="День тижня:")
+    day_of_week_label.pack()
+    day_of_week_entry = ttk.Combobox(
+        add_schedule_window,
+        values=[
+            "Понеділок", "Вівторок", "Середа", "Четвер", "П'ятниця", "Субота",
+            "Неділя"
+        ]
+    )
+    day_of_week_entry.pack()
+
+    even_odd_label = Label(add_schedule_window, text="Парний тиждень:")
+    even_odd_label.pack()
+    even_odd_entry = ttk.Combobox(add_schedule_window, values=["Так", "Ні"])
+    even_odd_entry.pack()
+
+    def save_new_schedule():
+        indexes = [el.current() for el in [subject_entry, group_entry, professor_entry, classroom_entry, time_entry, day_of_week_entry, even_odd_entry]]
+        # start_time = time(int(start_time_hours.get()), int(start_time_minutes.get()))
+        # end_time = time(int(end_time_hours.get()), int(end_time_minutes.get()))
+        if any(i == -1 for i in indexes):
+            messagebox.showerror("Помилка!", f"Існують незаповнені поля!")
+            return
+        times = ["8:40", "10:15", "10:35", "12:10", "12:20", "13:55", "14:05", "15:40"]
+        start_time = datetime.strptime(times[2*indexes[4]], "%H:%M")
+        end_time = datetime.strptime(times[2*indexes[4] + 1], "%H:%M")
+
+        # SQL запит на додавання викладача до таблиці Professors
+        query = "INSERT INTO new_schema.Schedule (SubjectId, GroupId, ProfessorId, ClassroomId, StartTime, EndTime, DayOfWeek, EvenOdd) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)"
+        values = (
+            subject_ids[indexes[0]],
+            groups_ids[indexes[1]],
+            professors_ids[indexes[2]],
+            classrooms_ids[indexes[3]],
+            start_time.strftime("%H:%M"),
+            end_time.strftime("%H:%M"),
+            indexes[5],
+            indexes[6]
+        )
+        cursor.execute(query, values)
+
+        messagebox.showinfo("Увага!", f"Запис успішно додано!")
+
+        # Закриття вікна
+        add_schedule_window.destroy()
+
+
+    save_button = Button(
+        add_schedule_window,
+        text="Зберегти",
+        command=save_new_schedule
+    )
+    save_button.pack()
+
+
+
+# print(get_subjects())
 main_window()
 
 cursor.close()
